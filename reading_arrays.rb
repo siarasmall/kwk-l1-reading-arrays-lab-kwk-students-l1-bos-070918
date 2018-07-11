9bfbfc72f0ffd72b5220bf2d8b09bbf03ce4a85b
siarasmall <siarasmall19@gmail.com>
2018-07-11 14:36:26 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -34,5 +34,5 @@ end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
-  return STUDENT_NAMES[0, 1, 2]
+  return STUDENT_NAMES[0..2]
 end
